a491c0d0e53ff478aa800af8cfbba7c1e0c59b1e
David-F-98 <u2903149@unimilitar.edu.co>
2024-02-01 07:01:13 -0500

    Rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes} from 'react-router-dom';
+import PaginaDeInicio from './Components/PaginaDeInicio';
+import Blog from './Components/Blog';
+import AcercaDe from './Components/AcercaDe';
+// Para trabajar con Reac Route tener en cuenta:
+// Usar BrowserRouter, NavLink, Route, Routes
+//  1. Encerrar dentro de BrowserRouter toda la estructura
+//  2. En vez de usaar <a href=''> </a>   USAR  <NavLink to=""></ NavLink> 
+//  3. Dentro del main usar la etiqueta <Routes> para que no se recargue toda la página
+//  4. La estructura principal que debe tener cada ruta que vayamos a tener va a ser: <Route path='#' element={} />
 const App = () => {
   return (
-    <div>
-      <h1>Mi Blog Personal</h1>
-    </div>
-    
+    <BrowserRouter>
+      <div>
+        <header>
+          <h1>Mi Blog Personal</h1>
+          <nav>
+            <NavLink to="/">Inicio</NavLink>
+            <NavLink to="/blog">Blog</NavLink>
+            <NavLink to="/acerca-de">Acerca de</NavLink>
+          </nav>
+        </header>
+        <main>
+          <Routes>
+            <Route  path='/' element={
+              <PaginaDeInicio/>
+            }/>
+            <Route path='/blog' element={
+              <Blog/>
+            }/>
+            <Route path='/acerca-de' element={
+              <AcercaDe/>
+            }/>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
  
---
+++ src/Components/AcercaDe.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+const AcercaDe = () => {
+    return ( 
+        <div>
+            <h2>Acerca de</h2>
+            <p>Hola me llamo David!</p>
+        </div>
+     );
+}
+ 
+export default AcercaDe;
